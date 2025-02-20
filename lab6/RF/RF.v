dbe3f1595097dcf9fa16c5884e73cf58af728d78
Ling <github@ling.moe>
2025-02-20 02:20:51 -0800

    change the register file to be asynchronous

--- lab6/RF/RF.v
+++ lab6/RF/RF.v
@@ -1,37 +1,35 @@
 module RF
 (
-    input clk,
-    input rst,
-    input wena,
-    input [63:0] wdata,
-    input [2:0] waddr,
-    input [2:0] r0addr,
-    input [2:0] r1addr,
+    // input clk,              // Clock
+    input rst,              // Reset
+    input Write_Enable,     // Write Enable
+    input [63:0] wdata,     // Data to Be Written
+    input [2:0] waddr,      // Write Data Address
+    input [2:0] rA_Addr,    // Output Data Address for Port A
+    input [2:0] rB_Addr,    // Output Data Address for Port B
 
-    output reg [63:0] r0data,
-    output reg [63:0] r1data
+    output reg [63:0] rA_Data,  // Output Data Port for Port A
+    output reg [63:0] rB_Data   // Output Data Port for Port B
 );
 
     reg [63:0] RF [7:0];
-	 
 	integer i;
-
-    always @(posedge clk) begin
-        if (rst == 1)
-        begin
-            for (i = 0; i < 8; i = i + 1) begin
-                RF[i] <= 64'b0;
-            end
-        end
-        else if (wena == 1)
-        begin
-            RF[waddr] <= wdata;
-        end
-    end
-
+    
     always @(*) begin
-        r0data = RF[r0addr];
-        r1data = RF[r1addr];
+        // RF[0] = 64'b0;  // Register 0 hardcodes 0
+
+        if(rst) begin
+            for (i = 0; i < 8; i = i + 1) begin
+                RF[i] = 64'b0;
+            end
+        end else if(Write_Enable) begin
+            RF[waddr] = wdata
+        end
+
+        // Port A and Port B Data Output
+        rA_Data = RF[rA_Addr];
+        rB_Data = RF[rB_Addr];
+
     end
 
 endmodule
